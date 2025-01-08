a2745d70d77dd0956185fb37fe72dc8691ee7aa0
DannyS032 <sinderdanny@gmail.com>
2025-01-08 20:17:46 +0200

    updated dataloader to load L and SNR metadata as well

--- data/dataset.py
+++ data/dataset.py
@@ -13,6 +13,14 @@ class ChannelDataset(Dataset):
             self.cir_h = np.transpose(f['CIR_High'], (2, 1, 0))
             self.cfr_h = np.transpose(f['CFR_High'], (2, 1, 0))
             self.toa = np.transpose(f['ToA'])
+            if 'L' in f:
+                self.l = np.transpose(f['L'])
+            else:
+                self.l = None
+            if 'SNR' in f:
+                self.snr = np.transpose(f['SNR'])
+            else:
+                self.snr = None
 
     def __len__(self):
         # Return length i.e. number of samples
@@ -25,14 +33,20 @@ class ChannelDataset(Dataset):
         cfr_h = self.cfr_h[index]
         toa = self.toa[index].squeeze()
 
-        sample = {'cir_l': cir_l, 'cir_h': cir_h,'cfr_h': cfr_h, 'toa': toa}
+        if hasattr(self, 'l') and hasattr(self, 'snr'):
+            l = self.l[index].squeeze()
+            snr = self.snr[index].squeeze()
+
+            sample = {'cir_l': cir_l, 'cir_h': cir_h,'cfr_h': cfr_h, 'toa': toa, 'l': l, 'snr': snr}
+        else:
+            sample = {'cir_l': cir_l, 'cir_h': cir_h,'cfr_h': cfr_h, 'toa': toa}
 
         return sample
 
 if __name__ == '__main__':
 
     proj_directory = os.getcwd()
-    file_path = os.path.join(proj_directory, 'data/train_data_high.h5')
+    file_path = os.path.join(proj_directory, 'data/test_data_high.h5')
     dataset = ChannelDataset(file_path)
 
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=1, pin_memory=True)
@@ -45,6 +59,8 @@ if __name__ == '__main__':
         print(f"cfr_h shape: {batch['cfr_h'].shape}")
         print(f"toa shape: {batch['toa'].shape}")
         print(f"ToA: {batch['toa'][0]}")
+        print(f"L: {batch['l'][0]}")
+        print(f"SNR: {batch['snr'][0]}")
 
         if i==5:
             break
